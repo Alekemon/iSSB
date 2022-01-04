82bf94ecf4c025c196927173b1742a69a7dc2d8a
Alekemon <42167718+Alekemon@users.noreply.github.com>
2022-01-04 13:36:20 +0100

    added green hill soundtrack

--- Source/SmashBros/Menus/Menus.cpp
+++ Source/SmashBros/Menus/Menus.cpp
@@ -198,6 +198,8 @@ namespace SmashBros
 		MusicManager::addSong("Zelda Theme", "Music/Legend of Zelda/Main Theme.ogg");
 		MusicManager::addSong("Zelda Theme Remix", "Music/Legend of Zelda/Main Theme Remix.ogg");
 		
+		MusicManager::addSong("Green Hill Zone", "Music/Sonic the Hedgehog/Green Hill Zone.mp3");
+		
 		MusicManager::loadSong("Main Theme");
 		
 		//Loading sounds
